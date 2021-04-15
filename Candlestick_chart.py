5d00887fd9fede34c42cd3f09605cc72685189bf
Mike Zubko <mishau7@gmail.com>
2021-04-16 01:40:09 +0300

    initial commit

--- Candlestick_chart.py
+++ Candlestick_chart.py
@@ -6,7 +6,8 @@ import time
 
 share = 'AMZN'
 t = round(time.time())
-url = f'https://query1.finance.yahoo.com/v7/finance/download/{share}?period1={t - 31536000}&period2={t}&interval=1d&events=history&includeAdjustedClose=true'
+period = float(input('Enter a period (years): '))
+url = f'https://query1.finance.yahoo.com/v7/finance/download/{share}?period1={round(t - 31536000 * period)}&period2={t}&interval=1d&events=history&includeAdjustedClose=true'
 
 file_path = os.path.join('data')
 os.makedirs(file_path, exist_ok=True)
@@ -21,9 +22,11 @@ df.Date = pd.to_datetime(df.Date)
 # df.info()
 df = df.set_index('Date')
 # print(df)
-
+plottype = 'candle'
+if period > 2:
+    plottype = 'line'
 # mpf.plot(df, volume=True, tight_layout=True)
 # mpf.plot(df, volume=True, type='line', tight_layout=True)
 # mpf.plot(df['2021-01'], volume=True, tight_layout=True)
 # mpf.plot(df['2021-01':], type='candle', mav=10, volume=True, tight_layout=True)
-mpf.plot(df, type='candle', mav=10, volume=True, tight_layout=True)
+mpf.plot(df, type=plottype, mav=10, volume=True, tight_layout=True)
